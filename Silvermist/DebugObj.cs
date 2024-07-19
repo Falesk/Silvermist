c37d3d8c8f02be540fc7e178f395e43f994d8b0a
Falesk <makskoval2802@gmail.com>
2024-07-19 21:14:30 +0300

    Круг из треугольников

--- Silvermist/DebugObj.cs
+++ Silvermist/DebugObj.cs
@@ -6,12 +6,14 @@ namespace Silvermist
     public class DebugObj : PhysicalObject, IDrawable
     {
         public Vector2 rotation, lastRotation;
+        public Vector2[,] points;
+        public SharedPhysics.TerrainCollisionData scratchTerrainCollisionData;
         public float darkness, lastDarkness;
         public int segments;
 
         public DebugObj(AbstractPhysicalObject abstr) : base(abstr)
         {
-            bodyChunks = new BodyChunk[] { new BodyChunk(this, 0, Vector2.zero, 15, 0.05f) };
+            bodyChunks = new BodyChunk[] { new BodyChunk(this, 0, Vector2.zero, 30, 0.05f) };
             bodyChunkConnections = new BodyChunkConnection[0];
             gravity = 0.9f;
             airFriction = 0.999f;
@@ -21,6 +23,7 @@ namespace Silvermist
             bounce = 0.1f;
             buoyancy = 0.9f;
             segments = 8;
+            points = new Vector2[segments * 2, 3];
         }
 
         public override void Update(bool eu)
@@ -29,8 +32,25 @@ namespace Silvermist
             lastRotation = rotation;
             if (firstChunk.ContactPoint.y < 0)
             {
-                rotation = (rotation - Custom.PerpendicularVector(rotation) * 0.1f * firstChunk.vel.x).normalized;
+                rotation = (rotation - Custom.PerpendicularVector(rotation) * 0.05f * firstChunk.vel.x).normalized;
                 firstChunk.vel.x *= 0.8f;
+            }
+            for (int i = 0; i < points.GetLength(0); i++)
+            {
+                float angle = Vector2.Angle(lastRotation, rotation) * ((firstChunk.vel.x > 0) ? -1 : 1);
+                points[i, 1] = points[i, 0];
+                points[i, 0] = FCustom.RotateVector(points[i, 0], angle);
+                points[i, 2] *= airFriction;
+                points[i, 2].y -= gravity;
+            }
+            for (int i = 0; i < points.GetLength(0); i++)
+            {
+                SharedPhysics.TerrainCollisionData data = scratchTerrainCollisionData.Set(points[i, 0], points[i, 1], points[i, 2], 1f, default, firstChunk.goThroughFloors);
+                data = SharedPhysics.VerticalCollision(room, data);
+                data = SharedPhysics.HorizontalCollision(room, data);
+                data = SharedPhysics.SlopesVertically(room, data);
+                points[i, 0] = data.pos;
+                points[i, 2] = data.vel;
             }
         }
 
@@ -38,6 +58,21 @@ namespace Silvermist
         {
             base.PlaceInRoom(placeRoom);
             firstChunk.HardSetPosition(placeRoom.MiddleOfTile(abstractPhysicalObject.pos));
+            rotation = Custom.RNV();
+            lastRotation = rotation;
+            ResetPoints();
+        }
+
+        public void ResetPoints()
+        {
+            for (int i = 0; i < points.GetLength(0); i++)
+            {
+                float ang = 360f * i / points.GetLength(0);
+
+                points[i, 0] = FCustom.RotateVector(Custom.PerpendicularVector(rotation), ang);
+                points[i, 1] = points[i, 0];
+                points[i, 2] *= 0f;
+            }
         }
 
         public void InitiateSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
@@ -45,7 +80,6 @@ namespace Silvermist
             sLeaser.sprites = new FSprite[]
             {
                 TriangleMesh.MakeLongMesh(segments, false, true),
-                new FSprite("Circle20") { scale = 0.4f, anchorY = 0f }
             };
             AddToContainer(sLeaser, rCam, null);
         }
@@ -53,7 +87,6 @@ namespace Silvermist
         public void DrawSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
         {
             Vector2 pos = Vector2.Lerp(firstChunk.lastPos, firstChunk.pos, timeStacker);
-            Vector2 rt = Custom.PerpendicularVector(Vector3.Slerp(lastRotation, rotation, timeStacker));
             lastDarkness = darkness;
             darkness = rCam.room.Darkness(pos) * (1f - rCam.room.LightSourceExposure(pos));
             if (darkness != lastDarkness)
@@ -61,19 +94,17 @@ namespace Silvermist
 
             TriangleMesh mesh = sLeaser.sprites[0] as TriangleMesh;
             Vector2 v = pos - camPos;
-            for (int i = 0; i < segments; i++)
+            for (int i = 0; i < points.GetLength(0); i += 2)
             {
-                float ang = i / (float)segments * 360f;
-                float angRad = ang * Mathf.PI / 180f;
-                float angNext = (i + 1) / (float)segments * 360f;
-
-                mesh.MoveVertice(i * 4, v + new Vector2(rt.x * Mathf.Cos(angRad) - rt.y * Mathf.Sin(angRad), rt.x * Mathf.Sin(angRad) + rt.y * Mathf.Cos(angRad)) * 15f);
-                mesh.MoveVertice(i * 4 + 1, v);
-                mesh.MoveVertice(i * 4 + 2, v + FCustom.RotateVector(rt, ang - 180f / segments) * 15f);
-                mesh.MoveVertice(i * 4 + 3, v + FCustom.RotateVector(rt, angNext) * 15f);
+                int ind = i / 2;
+                Vector2 v1 = Vector2.Lerp(points[i, 1], points[i, 0], timeStacker);
+                Vector2 v2 = Vector2.Lerp(points[i + 1, 1], points[i + 1, 0], timeStacker);
+                Vector2 v3 = Vector2.Lerp(points[(points.GetLength(0) - i > 2) ? i + 2 : 0, 1], points[(points.GetLength(0) - i > 2) ? i + 2 : 0, 0], timeStacker);
+                mesh.MoveVertice(ind * 4, v + v1 * 30f);
+                mesh.MoveVertice(ind * 4 + 1, v);
+                mesh.MoveVertice(ind * 4 + 2, v + v2 * 30f);
+                mesh.MoveVertice(ind * 4 + 3, v + v3 * 30f);
             }
-            float angle = Custom.VecToDeg((Vector2)Futile.mousePosition - v) - 90f;
-            sLeaser.sprites[1].SetPosition(v);
 
             if (slatedForDeletetion || rCam.room != room)
                 sLeaser.CleanSpritesAndRemove();
@@ -83,16 +114,12 @@ namespace Silvermist
         {
             TriangleMesh mesh = sLeaser.sprites[0] as TriangleMesh;
             for (int i = 0; i < mesh.verticeColors.Length; i++)
-            {
-                Color color = (i % 4 == 1) ? Color.red : Color.green;
-                mesh.verticeColors[i] = Color.Lerp(color, palette.blackColor, darkness);
-            }
-            sLeaser.sprites[1].color = Color.red;
+                mesh.verticeColors[i] = Color.Lerp((i % 4 == 1) ? Color.red : Color.green, palette.blackColor, darkness);
         }
 
         public void AddToContainer(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, FContainer newContatiner)
         {
-            newContatiner = newContatiner ?? rCam.ReturnFContainer("Background");
+            newContatiner = newContatiner ?? rCam.ReturnFContainer("Items");
             foreach (FSprite sprite in sLeaser.sprites)
             {
                 sprite.RemoveFromContainer();
--- Silvermist/FCustom.cs
+++ Silvermist/FCustom.cs
@@ -13,35 +13,47 @@ namespace Silvermist
 
         public static Vector2 RotateVector(Vector2 vector, float ang)
         {
-            float r = ang * Mathf.PI / 180f;
-            Matrix2x2 matrix = new Matrix2x2(Mathf.Cos(r), Mathf.Sin(r) * ((ang > 0) ? -1 : 1), Mathf.Sin(r) * ((ang > 0) ? 1 : -1), Mathf.Cos(r));
+            float r = Mathf.Abs(ang * Mathf.PI / 180f);
+            int m = (ang > 0) ? 1 : -1;
+            Matrix2x2 matrix = new Matrix2x2(
+                Mathf.Cos(r), m * -Mathf.Sin(r),
+                Mathf.Sin(r) * m, Mathf.Cos(r));
             return matrix * vector;
         }
 
         public struct Matrix2x2
         {
-            public Vector2 upper, lower;
+            public Vector2 left, right;
             private readonly float _a, _b, _c, _d;
             public float A => _a;
             public float B => _b;
             public float C => _c;
             public float D => _d;
 
-            public Matrix2x2(Vector2 upper, Vector2 lower) : this(upper.x, upper.y, lower.x, lower.y)
+            public Matrix2x2(Vector2 left, Vector2 right) : this(left.x, right.y, left.x, right.y)
             {
             }
             public Matrix2x2(float a, float b, float c, float d)
             {
                 (_a, _b, _c, _d) = (a, b, c, d);
-                upper = new Vector2(a, b);
-                lower = new Vector2(c, d);
+                left = new Vector2(a, c);
+                right = new Vector2(b, d);
             }
 
+            public override bool Equals(object obj) => obj is Matrix2x2 m && m == this;
+            public override string ToString() => $"|{A} {B}|\n|{C} {D}|";
+            public override int GetHashCode() => base.GetHashCode();
+
             public static Vector2 operator *(Matrix2x2 matrix, Vector2 vector) => new Vector2(vector.x * matrix.A + vector.y * matrix.B, vector.x * matrix.C + vector.y * matrix.D);
-            public static Vector2 operator *(Vector2 vector, Matrix2x2 matrix) => new Vector2(vector.x * matrix.A + vector.y * matrix.B, vector.x * matrix.C + vector.y * matrix.D);
+            public static Vector2 operator *(Vector2 vector, Matrix2x2 matrix) => matrix * vector;
             public static Matrix2x2 operator *(Matrix2x2 matrix, float d) => new Matrix2x2(matrix.A * d, matrix.B * d, matrix.C * d, matrix.D * d);
-            public static Matrix2x2 operator *(float d, Matrix2x2 matrix) => new Matrix2x2(matrix.A * d, matrix.B * d, matrix.C * d, matrix.D * d);
+            public static Matrix2x2 operator *(float d, Matrix2x2 matrix) => matrix * d;
             public static Matrix2x2 operator *(Matrix2x2 m1, Matrix2x2 m2) => new Matrix2x2(m1.A * m2.A + m1.B * m2.C, m1.A * m2.B + m1.C * m2.D, m1.C * m2.A + m1.D * m2.C, m1.C * m2.B + m1.D * m2.D);
+            public static Matrix2x2 operator +(Matrix2x2 m1, Matrix2x2 m2) => new Matrix2x2(m1.A + m2.A, m1.B + m2.B, m1.C + m2.C, m1.D + m2.D);
+            public static Matrix2x2 operator -(Matrix2x2 m1, Matrix2x2 m2) => new Matrix2x2(m1.A - m2.A, m1.B - m2.B, m1.C - m2.C, m1.D - m2.D);
+            public static Matrix2x2 operator /(Matrix2x2 matrix, float d) => new Matrix2x2(matrix.A / d, matrix.B / d, matrix.C / d, matrix.D / d);
+            public static bool operator ==(Matrix2x2 m1, Matrix2x2 m2) => m1.A == m2.A && m1.B == m2.B && m1.C == m2.C && m1.D == m2.D;
+            public static bool operator !=(Matrix2x2 m1, Matrix2x2 m2) => !(m1 == m2);
         }
     }
 }
